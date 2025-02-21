8605dec807abd091664d73af5d43d6208c148479
S0dxu <159276909+S0dxu@users.noreply.github.com>
2025-02-21 21:35:19 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const getRandomImage = require("./api/get-random-image");
+const getImageById = require("./api/get-image-by-id");
+
 const app = express();
-
 app.use(cors({ origin: "*" }));
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend running on Vercel!");
 });
+
+app.use("/", getRandomImage);
+app.use("/", getImageById);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
