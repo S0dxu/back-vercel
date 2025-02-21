867c728183d4c872561a5a20a66906c0771e183e
S0dxu <159276909+S0dxu@users.noreply.github.com>
2025-02-21 21:44:29 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -6,15 +6,23 @@ const getRandomImage = require("./api/get-random-image");
 const getImageById = require("./api/get-image-by-id");
 
 const app = express();
-app.use(cors({ origin: ["*"] }));
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend running on Vercel!");
 });
 
-app.use("/", getRandomImage);
-app.use("/", getImageById);
+app.use("/get-random-image", getRandomImage);
+app.use("/get-image-by-id", getImageById);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
